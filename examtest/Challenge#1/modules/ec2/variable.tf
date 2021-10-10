831cb3c0c3dd3d5c3098b14730fde62a1a9b0956
Rohit Patil <rohit.patil@adcolony.com>
2021-10-10 12:31:13 +0530

    added kpmg test details

---
+++ examtest/Challenge#1/modules/ec2/variable.tf
@@ -0,0 +1,37 @@
+###############################################
+# Common Variables
+###############################################
+variable "zone" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+}
+variable "monitoring" {
+  type = string
+  default = "false"
+}
+variable "ec2_subnet_id" {
+  type = string
+  default = "false"
+}
+
+variable "instnace_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "ec2_subnet_id" {
+type = "string"
+}
+
+variable "access_cidr" {
+  type = "string"
+}
